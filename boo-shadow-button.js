b047d1aece6834d1a451e3ab534b4779b9387b36
jxlxxsxn <juliachensun@gmail.com>
2021-04-02 14:22:13 -0700

    format/new recipe/boobtn

---
+++ boo-shadow-button.js
@@ -0,0 +1,12 @@
+$(".boo-shadow-button").click(function(){
+    $(".boo-toque").toggle();
+    $(".balboo-spatula").toggle();
+    $(".boo-shadow-button2").toggle();
+    $("this").toggle();
+})
+
+$(".boo-shadow-button2").click(function(){
+    $(".boo-toque").toggle();
+    $(".balboo-spatula").toggle();
+    $(".boo-shadow-button2").toggle();
+})
